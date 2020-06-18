1aec0eaa4551530a654d46d873e5bc036403acae
ledangkhoalt <ledangkhoalt@gmail.com>
2020-06-18 12:04:23 +0700

    [khoa] add guess game

---
+++ client/src/game.rs
@@ -0,0 +1,25 @@
+use rand::prelude::*;
+use std::cmp::Ordering;
+use std::io;
+pub fn run() {
+  let mut rng = rand::thread_rng();
+  let serrect = rng.gen_range(0, 100);
+  println!("Random number: {}", serrect);
+
+  loop {
+    let mut input = String::new();
+    println!("Say something:");
+    let mut _new_num = io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+
+    let guess: u32 = input.trim().parse().expect("Please type a number!");
+    println!("Random number: {}", guess);
+
+    match guess.cmp(&serrect) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => println!("You win!"),
+    }
+  }
+}
