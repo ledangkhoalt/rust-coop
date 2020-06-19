bc629566e7990981e130ed5ab9ada1374c59dc8f
KhoaLD <KhoaLD>
2020-06-19 22:28:47 +0700

    [khoa] client <=> server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,28 @@
-mod game;
+extern crate tungstenite;
+extern crate url;
+#[macro_use] 
+extern crate log;
+
+use tungstenite::{connect, Message};
+use url::Url;
+use log::Level;
+use std::io;
+//mod game;
 fn main() {
-    game::run();
+    //game::run();
+    env_logger::init();
+
+    let (mut socket, response) = connect(Url::parse("ws://127.0.0.1:3001").unwrap()).expect("aaaa");
+
+    println!("Connected to server");
+
+    loop {
+        let mut input = String::new();
+        println!("Say something:");
+        let mut _new_num = io::stdin()
+          .read_line(&mut input)
+          .expect("Failed to read line");
+          socket.write_message(Message::Text(input.into())).unwrap();
+    }
+    
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,22 @@
+extern crate tungstenite;
+
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::server::accept;
 fn main() {
-    println!("Hello, world!");
+
+    let server = TcpListener::bind("127.0.0.1:3001").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+
+                if msg.is_binary() || msg.is_text() {
+                    println!("{}", msg);
+                }
+            }
+        });
+    }
 }
